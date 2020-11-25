10b51883428e34e8e17b59537e0c172ffcc7fa65
EsaFazal <esaf@trabeya.com>
2020-11-25 10:11:54 +0530

    implemented redux persistent storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,28 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
-import cartReducer from "./cart/cartReducer";
+import cartReducer from "./cart/cart.reducers";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import directoryReducer from "./directory/directory.reducers";
 
-export default combineReducers({
+//redux persist config
+const persistConfig = {
+    key: 'root',
+    // type localstorage
+    storage,
+    // the reducers that we want persisted
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 });
+
+// export default combineReducers({
+//     user: userReducer,
+//     cart: cartReducer
+// });
+
+export default persistReducer(persistConfig, rootReducer);
